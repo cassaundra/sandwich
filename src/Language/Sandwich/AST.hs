1482cef6bfe6f35d0b0e4b62b39db5d1d4be6030
cassaundra <cass@cassaundra.io>
2022-06-15 20:12:53 -0700

    Implement parser

--- src/Language/Sandwich/AST.hs
+++ src/Language/Sandwich/AST.hs
@@ -8,42 +8,35 @@ type Identifier = String
 
 data Declaration
   = ConstantDecl Identifier Expression
-  | FunctionDecl Identifier FunctionSignature Block
+  | FunctionDecl Identifier SignatureType Block
   deriving (Show, Eq)
 
 data Block = Block [Statement] (Maybe Expression)
   deriving (Show, Eq)
 
-data FunctionSignature =
-  FunctionSignature
-    { funSigArguments  :: [(Identifier, Maybe Type)]
-    , funSigReturnType :: Type
-    }
-  deriving (Show, Eq)
-
 data Statement
   = DeclarationStmt Declaration
-  | AssignmentStmt [(AssignmentIdentifier, Expression)] -- should be non-empty
-  | OperatorAssignmentStmt BinaryOperand AssignmentIdentifier Expression
   | ExpressionStmt Expression
+  | AssignmentStmt [(AssignmentIdentifier, Expression)] -- should be non-empty
+  | OperatorAssignmentStmt AssignmentIdentifier Expression
   | IfStmt IfStatement
   | WhileStmt ConditionalBlock
-  | ReturnStmt Expression
+  | ReturnStmt (Maybe Expression)
   | BreakStmt
   | ContinueStmt
   deriving (Show, Eq)
 
 type ConditionalBlock = (Expression, Block)
 
-data IfStatement =
-  IfStatement
-    { ifBlocks  :: [ConditionalBlock] -- should be non-empty
-    , elseBlock :: Maybe ConditionalBlock
-    }
+data IfStatement = IfStatement
+  { ifBlocks  :: [ConditionalBlock], -- should be non-empty
+    elseBlock :: Maybe ConditionalBlock
+  }
   deriving (Show, Eq)
 
 data AssignmentIdentifier
   = SimpleIdent Identifier
+  | TypedIdent Identifier Type
   | IndexedIdent Identifier Int
   deriving (Show, Eq)
 
@@ -52,32 +45,46 @@ data Expression
   | ArrayExpr [Expression]
   | CallExpr Expression [Expression]
   | TernaryExpr Expression Expression Expression
-  | OperationExpr Operation
   | IndexExpr Expression Int
+  | OperationExpr Operation
   | VariableExpr Identifier
   | LiteralExpr Literal
   deriving (Show, Eq)
 
+-- data Operation
+--   = UnaryOp UnaryOperand Expression
+--   | BinaryOp BinaryOperand Expression Expression
+--   deriving (Show, Eq)
+
 data Operation
-  = UnaryOp UnaryOperand Expression
-  | BinaryOp BinaryOperand Expression Expression
+  = RefOp Expression
+  | NegOp Expression
+  | MulOp Expression Expression
+  | DivOp Expression Expression
+  | AddOp Expression Expression
+  | SubOp Expression Expression
+  | BitAndOp Expression Expression
+  | BitOrOp Expression Expression
+  | BitXorOp Expression Expression
+  | BitShlOp Expression Expression
+  | BitShrOp Expression Expression
   deriving (Show, Eq)
 
-data UnaryOperand = NegateOp | ReferenceOp
-  deriving (Show, Eq)
+-- data UnaryOperand = NegOp | RefOp
+--   deriving (Show, Eq)
 
-data BinaryOperand
-  = AddOp
-  | SubOp
-  | MulOp
-  | DivOp
-  | ModOp
-  | BitAndOp
-  | BitOrOp
-  | BitXorOp
-  | BitShlOp
-  | BitShrOp
-  deriving (Show, Eq)
+-- data BinaryOperand
+--   = AddOp
+--   | SubOp
+--   | MulOp
+--   | DivOp
+--   | ModOp
+--   | BitAndOp
+--   | BitOrOp
+--   | BitXorOp
+--   | BitShlOp
+--   | BitShrOp
+--   deriving (Show, Eq)
 
 data Literal
   = IntegerLit Int
@@ -87,9 +94,8 @@ data Literal
   | Lambda LambdaSignature Block
   deriving (Show, Eq)
 
-data LambdaSignature =
-  LambdaSignature
-    { lamSigArguments  :: [(Identifier, Maybe Type)]
-    , lamSigReturnType :: Maybe Type
-    }
+data LambdaSignature = LambdaSignature
+  { lamSigArguments  :: [(Identifier, Maybe Type)],
+    lamSigReturnType :: Maybe Type
+  }
   deriving (Show, Eq)
--- src/Language/Sandwich/Parser.hs
+++ src/Language/Sandwich/Parser.hs
@@ -1,3 +1,175 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Language.Sandwich.Parser where
 
+import           Control.Applicative            (many, optional, (<|>))
+import           Control.Monad.Combinators.Expr (Operator (..), makeExprParser)
+import           Data.Void                      (Void)
+import           Language.Sandwich.AST
+import           Language.Sandwich.Type
+import           Text.Megaparsec                (Parsec, choice, label,
+                                                 runParser)
+import           Text.Megaparsec.Char           (alphaNumChar, char, letterChar,
+                                                 space1)
+import qualified Text.Megaparsec.Char.Lexer     as L
 
+unimplemented = error "not implemented"
+
+type Parser = Parsec Void String
+
+pIdentifier :: Parser Identifier
+pIdentifier =
+  label "identifier" $
+    lexeme $
+      (:) <$> (letterChar <|> char '_') <*> many (alphaNumChar <|> char '_')
+
+pFile :: Parser Module
+pFile = many pDeclaration
+
+pDeclaration :: Parser Declaration
+pDeclaration = pConstantDecl <|> pFunctionDecl
+
+pConstantDecl :: Parser Declaration
+pConstantDecl = label "constant declaration" $ do
+  symbol "const"
+  ident <- pIdentifier
+  symbol "="
+  expr <- pExpression
+  return $ ConstantDecl ident expr
+
+pFunctionDecl :: Parser Declaration
+pFunctionDecl =
+  label "function declaration" $ do
+    symbol "func"
+    ident <- pIdentifier
+    sigType <- pSigType
+    block <- pBlock
+    return $ FunctionDecl ident sigType block
+
+pBlock :: Parser Block
+pBlock = surround '{' '}' $ do
+  body <- many pStatement
+  returnExpr <- optional pExpression
+  return $ Block body returnExpr
+
+pStatement :: Parser Statement
+pStatement =
+  choice
+    [ DeclarationStmt <$> pDeclaration,
+      ExpressionStmt <$> pExpression,
+      pAssignStmt,
+      pOpAssignStmt,
+      pIfStmt,
+      pWhileStmt,
+      pReturnStmt,
+      pBreakStmt,
+      pContinueStmt
+    ]
+    <* symbol ";"
+
+pAssignStmt :: Parser Statement
+pAssignStmt = unimplemented
+
+pOpAssignStmt :: Parser Statement
+pOpAssignStmt = unimplemented
+
+pIfStmt :: Parser Statement
+pIfStmt = unimplemented
+
+pWhileStmt :: Parser Statement
+pWhileStmt = unimplemented
+
+pReturnStmt :: Parser Statement
+pReturnStmt = ReturnStmt <$> (symbol "return" *> optional pExpression)
+
+pBreakStmt :: Parser Statement
+pBreakStmt = BreakStmt <$ symbol "break"
+
+pContinueStmt :: Parser Statement
+pContinueStmt = ContinueStmt <$ symbol "continue"
+
+pExpression :: Parser Expression
+pExpression =
+  choice
+    [ TupleExpr <$> surround '(' ')' (many pExpression),
+      ArrayExpr <$> surround '[' ']' (many pExpression),
+      pCallExpr,
+      -- TODO ternary
+      -- TODO index
+      pOperation,
+      VariableExpr <$> pIdentifier,
+      LiteralExpr <$> pLiteral
+    ]
+
+pCallExpr :: Parser Expression
+pCallExpr = do
+  fun <- pExpression
+  args <- many pExpression
+  return $ CallExpr fun args
+
+pOperation :: Parser Expression
+pOperation = makeExprParser pExpression operatorTable
+
+operatorTable :: [[Operator Parser Expression]]
+operatorTable =
+  [ [ prefix "&" RefOp,
+      prefix "-" NegOp,
+      Prefix (id <$ symbol "+")
+    ],
+    [ binary "*" MulOp,
+      binary "/" DivOp
+    ],
+    [ binary "+" AddOp,
+      binary "-" SubOp
+    ],
+    [ binary "&" BitAndOp,
+      binary "|" BitOrOp,
+      binary "^" BitXorOp
+    ],
+    [ binary "<<" BitShlOp,
+      binary ">>" BitShrOp
+    ]
+  ]
+
+-- adopted from the megaparsec tutorial
+
+binary :: String -> (Expression -> Expression -> Operation) -> Operator Parser Expression
+binary name f = InfixL (((OperationExpr .) <$> f) <$ symbol name)
+
+prefix :: String -> (Expression -> Operation) -> Operator Parser Expression
+prefix name f = Prefix ((OperationExpr <$> f) <$ symbol name)
+
+pLiteral :: Parser Literal
+pLiteral = unimplemented
+
+pType :: Parser Type
+pType =
+  label "type" $
+    choice
+      [ IntegerType <$ symbol "int",
+        FloatType <$ symbol "float",
+        BooleanType <$ symbol "bool",
+        StringType <$ symbol "string",
+        TupleType <$> surround '(' ')' (many pType),
+        ArrayType <$> surround '[' ']' pType,
+        RefType <$> (char '&' *> pType),
+        FunctionType <$> (symbol "fn" *> pSigType)
+      ]
+
+pSigType :: Parser SignatureType
+pSigType = do
+  sigArguments <- surround '(' ')' $ do unimplemented
+  sigReturnType <- pType
+  return SignatureType {..}
+
+whitespace :: Parser ()
+whitespace = L.space space1 (L.skipLineComment "//") (L.skipBlockComment "/*" "*/")
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme whitespace
+
+symbol :: String -> Parser String
+symbol = L.symbol whitespace
+
+surround :: Char -> Char -> Parser a -> Parser a
+surround l r a = lexeme (char l) *> a <* char r
